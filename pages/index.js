7a62fc135fd060c4bfc109a4103664539416b109
sunsunarto <sunaryo24259@outlook.com>
2025-11-05 11:38:34 +0700

    second commit

--- pages/index.js
+++ pages/index.js
@@ -14,14 +14,20 @@ export default function Home() {
         />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.js
+            Edit{" "}
+            <code className="font-medium text-foreground">
+              pages/index.js
             </code>
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            Save to reload.
+          </li>
+          <li className="tracking-[-.01em]">
+            <code className="font-medium text-foreground">
+              pages/index.js
+            </code>{" "}
+            has been opened in a new tab.
           </li>
         </ol>
 
@@ -39,7 +45,7 @@ export default function Home() {
               width={20}
               height={20}
             />
-            
+            Students table
           </a>
         </div>
       </main>
